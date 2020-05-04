2fd20b62300761b653340c4b7603bf9a7c09d0d8
ChizhStanislav <stas23041991@gmail.com>
2020-05-04 22:58:18 +0300

    Start project

---
+++ src/main/java/by/chyzh/beerpong/BeerpongApplication.java
@@ -0,0 +1,13 @@
+package by.chyzh.beerpong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeerpongApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeerpongApplication.class, args);
+    }
+
+}
---
+++ src/test/java/by/chyzh/beerpong/BeerpongApplicationTests.java
@@ -0,0 +1,13 @@
+package by.chyzh.beerpong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeerpongApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
