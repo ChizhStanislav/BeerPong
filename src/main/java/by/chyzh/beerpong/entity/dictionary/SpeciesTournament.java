38374c5124c69d51f9d64b77a7478e9891e9d817
ChizhStanislav <stas23041991@gmail.com>
2020-05-11 08:49:01 +0300

    add table region, data for tables(country, region, cities), region(entity, repository, service, dto, controller), role(organizer)

--- src/main/java/by/chyzh/beerpong/entity/dictionary/SpeciesTournament.java
+++ src/main/java/by/chyzh/beerpong/entity/dictionary/SpeciesTournament.java
@@ -1,4 +1,9 @@
 package by.chyzh.beerpong.entity.dictionary;
 
 public enum SpeciesTournament {
+
+    TWO_STAGE_WITH_PLAYOFF,
+    TWO_STAGE_WITH_FALL,
+    ONE_STAGE_WITH_PLAYOFF,
+    ONE_STAGE_WITH_FALL
 }
